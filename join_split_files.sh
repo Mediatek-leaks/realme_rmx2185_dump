8cbe17d720bc446721a877d9cd662e1f19b05e07
root <root@fv-az139-583.ko3klcwoayhujlaf5nvcnsniqe.cx.internal.cloudapp.net>
2021-03-04 08:40:08 +0000

    Add extras for full_oppo6765-user 10 QP1A.190711.020 68b77aba7cb33275 release-keys
    Signed-off-by: root <root@fv-az139-583.ko3klcwoayhujlaf5nvcnsniqe.cx.internal.cloudapp.net>

---
+++ join_split_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/product/app/webview/webview.apk.* 2>/dev/null >> system/system/product/app/webview/webview.apk
+rm -f system/system/product/app/webview/webview.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v29.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v29.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v29.apex.* 2>/dev/null
